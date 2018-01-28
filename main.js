aa8a85af28e1e55e38a25879d40703be4b61aa02
Marta <mchupil@mail.ru>
2018-01-29 01:29:11 +0200

    some changes

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block {
     constructor(index, timestamp, data, previousHash = '') {
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
-        this.hash = '';
+        this.hash = calculateHash();
+    }
+
+    calculateHash() {
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
     }
 }
+
+// class Blockchain {
+//     constructor()
+// }
